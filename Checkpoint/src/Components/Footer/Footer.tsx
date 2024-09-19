8fc444920c3421c98f7f3c18d37d8cd9865dd683
LucasLDC <LucasL.Chagas@outlook.com>
2024-09-19 09:08:39 -0300

    w

--- Checkpoint/src/Components/Footer/Footer.tsx
+++ Checkpoint/src/Components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () =>{
             <FundoFooter>
                 <h2>João Gabriel Boaventura Marques e Silva <span style={{fontStyle: 'italic'}}>Rm 554874</span></h2>
                 <h2>Kaian Gustavo de Oliveira Nascimento <span style={{fontStyle: 'italic'}}>Rm 558986</span></h2>
-                <h2>Lucas Leal das Chagas <span style={{fontStyle: 'italic'}}>RM551124</span></h2>
+                <h2>Lucas Leal das Chagas <span style={{fontStyle: 'italic'}}>Rm 551124</span></h2>
             </FundoFooter>
 
         </>
--- Checkpoint/src/Components/Header/Header.tsx
+++ Checkpoint/src/Components/Header/Header.tsx
@@ -1,23 +1,25 @@
 import { useLocation } from "react-router-dom";
 import { MenuLink } from "../../StyledComponents/Home";
-
+import { FundoHeader } from "../../StyledComponents/Header";
+ 
 const Header = () => {
     const UrlNome = useLocation();// coleto minha url
     return (
         <>
-
-            <h1>meu cabecario</h1>
-            {UrlNome.pathname !== "/Produtos" && (
-                <MenuLink to="/Produtos">Produtos</MenuLink>
-            )}
-
-            {UrlNome.pathname !== "/DetalhesProdutos" && (
-                <MenuLink to="/DetalhesProdutos">Produtos</MenuLink>
-            )}
+            <FundoHeader>
+                <h1>meu cabecario</h1>
+                {UrlNome.pathname !== "/Produtos" && (
+                    <MenuLink to="/Produtos">Produtos</MenuLink>
+                )}
+                {UrlNome.pathname !== "/DetalhesProdutos" && (
+                    <MenuLink to="/DetalhesProdutos">Detalhes Produtos</MenuLink>
+                )}
+            </FundoHeader>
         </>
-
-
+ 
+ 
     )
 }
-
+ 
 export default Header;
+ 
