e3a637c1a71357fdca5499f70ad46145ad56d24a
João Gabriel <jgab2005@live.com>
2024-09-19 09:24:01 -0300

    Update DetalhesPordutos.tsx

--- Checkpoint/src/Components/DetalhesProdutos/DetalhesPordutos.tsx
+++ Checkpoint/src/Components/DetalhesProdutos/DetalhesPordutos.tsx
@@ -6,7 +6,6 @@ const DetalhesProdutos = () => {
         <>
             <h2>detalhes produtos</h2>
             <Produtos nome="BATATA" preco="R$2,00" img="\img\BATATA.jpg" descricao="batata de qualidade"/>
-
             <Produtos nome="LEITE INTEGRAL" preco="R$ 5,00" img="\img\LEITE.jpg" descricao="Leite Integral"/>
         </>
     )
