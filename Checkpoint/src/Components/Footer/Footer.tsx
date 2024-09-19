98a8776aed83c7eda8721d72d80dc1800a74befc
João Gabriel <jgab2005@live.com>
2024-09-19 08:52:22 -0300

    .

--- Checkpoint/src/Components/Footer/Footer.tsx
+++ Checkpoint/src/Components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ const Footer = () =>{
     return(
         <>
             <FundoFooter>
-                <h2>Giovanna Revito Roz <span style={{fontStyle: 'italic'}}>Rm 558981</span></h2>
+                <h2>João Gabriel Boaventura Marques e Silva <span style={{fontStyle: 'italic'}}>Rm 554874</span></h2>
                 <h2>Kaian Gustavo de Oliveira Nascimento <span style={{fontStyle: 'italic'}}>Rm 558986</span></h2>
-                <h2>Lucas Leal das Chagas <span style={{fontStyle: 'italic'}}>RM</span></h2>
+                <h2>Lucas Leal das Chagas <span style={{fontStyle: 'italic'}}>RM551124</span></h2>
             </FundoFooter>
 
         </>
