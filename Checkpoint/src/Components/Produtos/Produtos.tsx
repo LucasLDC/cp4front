7c45771f12a83efe7dcb1766bee28e2b15f3cd74
LucasLDC <LucasL.Chagas@outlook.com>
2024-09-19 09:26:27 -0300

    q

--- Checkpoint/src/Components/Produtos/Produtos.tsx
+++ Checkpoint/src/Components/Produtos/Produtos.tsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
 
 interface products{
-    nome ? : string;
-    preco ? : string;
-    img ? : string;
-    descricao ? : string;
+    nome : string;
+    preco: string;
+    img: string;
+    descricao : string;
 }
 
 
-<<<<<<< HEAD
 const Produtos = ( props : products) => {
     const [contar, setCount] = useState(0);
-=======
-const Produtos = ( props : products ) => {
->>>>>>> 372834b2ab39f2f3d5e4fad12b138a51b24bd892
     return(
 
         <>
@@ -23,7 +19,7 @@ const Produtos = ( props : products ) => {
                 <p>{props.preco} {props.descricao}</p>
                 <p></p>
                 <button onClick={() => setCount((contar) => contar + 1)}>
-                count is {contar}
+                adicionar produto {contar}
                 </button>
             </div>
         </>
