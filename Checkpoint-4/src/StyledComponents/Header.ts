99495b936d516b79b4b031356c4ee9521e62aa51
João Gabriel <jgab2005@live.com>
2024-10-11 10:46:14 -0300

    .

--- Checkpoint-4/src/StyledComponents/Header.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
- 
-export const FundoHeader = styled.div`
-   
-        margin: 0;
-   
-        background-color: lightgray;
-`
--- Checkpoint-4/src/StyledComponents/Home.ts
+++
@@ -1,6 +0,0 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-export const MenuLink = styled(Link)`
-    color: darkRed;
-`
