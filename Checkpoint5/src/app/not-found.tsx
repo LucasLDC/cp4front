37c1f7569a1d7710c12fe64154f925da1b6a1a64
João Gabriel <jgab2005@live.com>
2024-10-11 10:45:05 -0300

    .

---
+++ Checkpoint5/src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import styles from './Error404.module.css';
+
+export default function NotFound() {
+    return (
+            <div className={styles.container}>
+            <h1 className={styles.title}>404</h1>
+            <p className={styles.message}>Oops! Página não encontrada.</p>
+            <button 
+                onClick={() => window.location.href = '/'}
+                className={styles.button}
+            >
+                Voltar para a página inicial
+            </button>
+        </div>
+    );
+}
