b0068a397e74b42cc07d664c80717244eae0fd59
João Gabriel <jgab2005@live.com>
2024-10-13 20:47:31 -0300

    POR FAVOR, IMPLEMENTEM O SESSION/LOCAL STORAGE NO LOGIN!

--- Checkpoint5/src/app/not-found.tsx
+++ Checkpoint5/src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>404</h1>
-            <p className={styles.message}>Oops! Página não encontrada.</p>
+            <p className={styles.message}>Oops! Uma pena não ser isso que você procura :( </p>
             <Link href="/Login">
                 <button className={styles.button} > Voltar para a página inicial</button>
             </Link>
