a28d02ba846dfc062279e85df36489f0952c1567
KaianL3g3nd <kaiangustavo20@gmail.com>
2024-10-14 11:20:22 -0300

    Update Header.tsx

--- Checkpoint5/src/app/Header/Header.tsx
+++ Checkpoint5/src/app/Header/Header.tsx
@@ -10,6 +10,7 @@ const Header = () => {
                     <ul className={styles.navUl}>
                         <li className={styles.navLi}><Link className={styles.navLink} href="/">Home</Link></li>
                         <li className={styles.navLi}><Link className={styles.navLink} href="/Login">Login</Link></li>
+                        <li className={styles.navLi}><Link className={styles.navLink} href="/Adicione_Servicos">adicione</Link></li>
                     </ul>
                 </nav>
             </header>
